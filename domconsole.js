dc17c79cc64e0ae59c6f1419ae3ee4c01ec5a83a
serapath (Alexander Praetorius) <dev@serapath.de>
2016-02-17 22:02:46 +0100

    REFACTOR factor out submodules

--- domconsole.js
+++ domconsole.js
@@ -1,3 +1,6 @@
+'use strict'
+var javascriptserialize = require('javascript-serialize')
+
 var style = [
   '#terminal {',
   '  box-sizing    : border-box;',
@@ -7,7 +10,7 @@ var style = [
   '  padding       : 15px 20px 15px 20px;',
   '  border-radius : 15px;',
   '  border        : 2px solid #CEE1F0;',
-  '  font-family   : Monaco;',
+  '  font-family   : Courier;',
   '  font-size     : 16px;',
   '  position      : absolute;',
   '  bottom        : 0;',
@@ -22,21 +25,20 @@ document.body.appendChild(t)
 
 var term = require('hypernal')()
 term.appendTo('#terminal')
-function tt (content) { term.write(content+'<br>') }
-var logger = { log: tt }
+var logger = { log: javascriptserialize }
 var init = false
 
-module.exports = function getLogger (opts) {
+function getLogger (opts) {
   opts = opts || {}
   if (opts.console && !init) {
     init = true
     var old = console.log.bind(console)
     function consoleLog() {
       old.apply(null, arguments)
-      var arr = [].slice.call(arguments)
-      tt(arr.join(' '))
+      var arr = javascriptserialize.apply(null, arguments)
+      term.write(arr.join('')+'<br><hr><br>')
     }
-    console.log = consoleLog
+     console.log = consoleLog
     logger.log = consoleLog
   }
   return logger

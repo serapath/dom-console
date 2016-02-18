caf0610a254610628d0fff6f0ef5aac006b8cc0a
serapath (Alexander Praetorius) <dev@serapath.de>
2016-02-18 20:27:19 +0100

    UPDATE readme to warn logging errors on requirebin

--- test/bundle.js
+++ test/bundle.js
@@ -8393,6 +8393,36 @@ module.exports = {
 },{}],22:[function(require,module,exports){
 require('..')({console:true})
 
+
+
+var Buffer = require('buffer').Buffer
+
+
+console.log(new Buffer(5))
+
+var x = { leaf: 'leaf' }
+x['aCircle'] = x
+console.log(x)
+
+console.log({a:'x', fn:function(x){return 5}})
+console.log(new Date)
+console.log({a:'b',c:[1,2,3],x:{y:{z:['a',{b:'c'}]}}})
+console.log(null)
+console.log(undefined)
+console.log("hey")
+console.log(false)
+console.log(true)
+console.log(function asdf () {})
+console.log(12)
+console.log(/asdf/)
+console.log((function(){ return arguments })(1,true))
+console.log([])
+console.log(document.createElement('div'))
+console.log(NaN)
+debugger
+console.log(new Error('Ups! Something wrong...'))
+
+
 console.error(new Error('Ups! Something wrong...'))
 console.log({a: '5'})
 console.error({a: '5'})
@@ -8406,4 +8436,4 @@ console.log('WWWWWWWWWW WWWWWWWWWW WWWWWWWWWW WWWWWWWWWW WWWWWWWWWW WWWWWWWWWW W
 function test (p) { var x = JSON.parse(p) }
 test(function(){})
 
-},{"..":6}]},{},[22]);
+},{"..":6,"buffer":2}]},{},[22]);
--- test/test.js
+++ test/test.js
@@ -1,5 +1,35 @@
 require('..')({console:true})
 
+
+
+var Buffer = require('buffer').Buffer
+
+
+console.log(new Buffer(5))
+
+var x = { leaf: 'leaf' }
+x['aCircle'] = x
+console.log(x)
+
+console.log({a:'x', fn:function(x){return 5}})
+console.log(new Date)
+console.log({a:'b',c:[1,2,3],x:{y:{z:['a',{b:'c'}]}}})
+console.log(null)
+console.log(undefined)
+console.log("hey")
+console.log(false)
+console.log(true)
+console.log(function asdf () {})
+console.log(12)
+console.log(/asdf/)
+console.log((function(){ return arguments })(1,true))
+console.log([])
+console.log(document.createElement('div'))
+console.log(NaN)
+debugger
+console.log(new Error('Ups! Something wrong...'))
+
+
 console.error(new Error('Ups! Something wrong...'))
 console.log({a: '5'})
 console.error({a: '5'})
